52272856fd2bd5774a09286b0a031b47045fcdf7
Makino0327 <k024g0008@g.neec.ac.jp>
2026-05-04 17:24:11 +0900

    first commit

---
+++ addons/level_editor.py
@@ -0,0 +1,26 @@
+import bpy
+
+# ブレンダーに認識するアドオン情報
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Taro Kanata",
+    "version": (1, 0),
+    "blender": (4, 0, 0),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+# アドオン有効化時コールバック
+def register():
+    print("レベルエディタが有効化されました。")
+
+# アドオン無効化時コールバック
+def unregister():
+    print("レベルエディタが無効化されました。")
+    
+if __name__=="__main__":
+    register()
